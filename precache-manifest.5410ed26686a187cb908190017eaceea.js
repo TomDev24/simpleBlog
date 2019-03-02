6eb572d478e00af6c10679ba2de85dd36fa7b1fb
TomDev24 <mail.com67747@gmail.com>
2019-03-02 12:52:18 +0300

    Updates

---
+++ precache-manifest.5410ed26686a187cb908190017eaceea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1371a1a66f12610a464",
+    "url": "/tomdev24.github.io/static/js/runtime~main.b1371a1a.js"
+  },
+  {
+    "revision": "0ff2c2601237a0e99e8e",
+    "url": "/tomdev24.github.io/static/js/main.0ff2c260.chunk.js"
+  },
+  {
+    "revision": "ad267140477f74b8ceb3",
+    "url": "/tomdev24.github.io/static/js/2.ad267140.chunk.js"
+  },
+  {
+    "revision": "0ff2c2601237a0e99e8e",
+    "url": "/tomdev24.github.io/static/css/main.b142c52e.chunk.css"
+  },
+  {
+    "revision": "c4829f92c0584e6d0593c1a78e92cdf8",
+    "url": "/tomdev24.github.io/index.html"
+  }
+];
